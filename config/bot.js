09799cc94ee01dedacc85f2464ba654df51221a3
givhome <79974727+givhome@users.noreply.github.com>
2021-03-05 00:06:30 +0300

    Update bot.js

--- config/bot.js
+++ config/bot.js
@@ -8,9 +8,9 @@ module.exports = {
     },
 
     discord: {
-        token: 'TOKEN',
-        prefix: 'PREFIX',
-        activity: 'ACTIVITY',
+        token: 'Nzk3NjIxMzMyMjQxOTQwNTAw.X_pIwA.uwJaOpeHLoCgMhaS1cAXk7Q_i3w',
+        prefix: 'c!',
+        activity: 'c!help',
     },
 
     filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
